16d017b3e9d45c2342a6132aa188283c39b78c51
makako3wr3fr <wellingtonpro6548@gmail.com>
2024-11-07 23:08:54 -0300

    Update comments.js

--- comments.js
+++ comments.js
@@ -1,13 +1,17 @@
-unction submitComment() {
-    const commentInput = document.querySelector('#comment-input textarea');
-    const commentsList = document.getElementById('comments-list');
+// Referências aos elementos
+const commentInput = document.getElementById('commentInput');
+const submitComment = document.getElementById('submitComment');
+const commentsSection = document.getElementById('commentsSection');
 
-    if (commentInput.value.trim()) {
+// Adicionar novo comentário
+submitComment.addEventListener('click', () => {
+    const commentText = commentInput.value.trim();
+    if (commentText) {
         const newComment = document.createElement('div');
         newComment.classList.add('comment');
-        newComment.innerHTML = `<p><strong>@nickname:</strong> ${commentInput.value}</p>`;
+        newComment.innerHTML = `<p><strong>@nickname:</strong> ${commentText}</p>`;
 
-        commentsList.appendChild(newComment);
+        commentsSection.appendChild(newComment);
         commentInput.value = '';
     }
-}
+});
